5cb49bdd453af2af2b8cc9f5c5d90768b20e8181
Consisten14 <905632925@qq.com>
2019-01-03 08:56:10 +0800

    33

---
+++ DotnetWindowsPlatform/IOHelper/FileHelper.cs
@@ -0,0 +1,25 @@
+﻿using System.IO;
+using System.Text;
+
+namespace DotnetWindowsPlatform.IOHelper
+{
+    public static class FileHelper
+    {
+        public static void WriteText(string saveTextDirectory, string saveTextName, string contents)
+        {
+            if (!Directory.Exists(saveTextDirectory))
+            {
+                Directory.CreateDirectory(saveTextDirectory);
+            }
+
+            var finalPath = Path.Combine(saveTextDirectory, saveTextName);
+
+            using (var fileStream = new FileStream(finalPath, FileMode.OpenOrCreate, FileAccess.ReadWrite))
+            {
+                var bytes = Encoding.Default.GetBytes(contents);
+
+                fileStream.Write(bytes, 0, bytes.Length);
+            }
+        }
+    }
+}
